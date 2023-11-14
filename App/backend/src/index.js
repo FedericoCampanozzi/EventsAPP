5668829f9205cd16a681e68b756b10f244bc65d8
Federico_Campanozzi <federico.campanozzi@gmail.com>
2023-11-14 22:36:01 +0100

    Implemented api with swagger-jsdoc and swagger-ui-express. Added "dev" script with nodemon.

--- App/backend/src/index.js
+++ App/backend/src/index.js
@@ -6,6 +6,9 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const eRouters = require('./routes/eventRoutes');
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}) );
 
@@ -21,5 +24,22 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', eRouters.routers);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info:{
+      title:'EVENTS APP API',
+      version:'1.0.0'
+    },
+    server:{
+      url:' http://localhost:8080'
+    }
+  },
+  apis:['./src/routes/*.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.listen(config.port, ()=> console.log('Server is listening on port ' + config.url ));
